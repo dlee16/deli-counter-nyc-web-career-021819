9ee9032c3a54adff00d223ef6359a75fa8b065dc
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-04 15:01:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(array)
 deli =[]
   for deli.each_with_index do |name, index|
-    return 
+    return "The line is currently #{index}."
   else 
     return "The line is currently empty."
 end 
