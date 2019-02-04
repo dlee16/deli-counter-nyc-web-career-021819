402685422fd54dff71c991ef6517cd9928c44c50
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-04 15:27:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,9 @@ deli =[]
 def line(deli)
   message= "The line is currently:"
   if deli.length >0 
-    return "The line is currently empty."
-  else
   deli.each.with_index(1) do |name, index|
-    line.push("#{index}. #{name}")
-    return line
-  end
-return "The line is currently" + line
+    puts message +="#{index}. #{name}"
+  else 
+puts "The line is currently empty."
 end
-end
+end 
