30a98a3ec60c22577df8f1e6923ed4a63963ce95
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-04 15:26:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ deli =[]
 
 def line(deli)
   message= "The line is currently:"
-  if line.length < deli.length
+  if deli.length >0 
     return "The line is currently empty."
   else
   deli.each.with_index(1) do |name, index|
