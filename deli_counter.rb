0f7e861d201b43b9dfab8da00296f04f999500e4
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-04 15:07:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(person)
 deli =[]
   for person.each_with_index do |name, index|
-    deli.push("The line is currently: #{index}.#{name}")
+    deli.push("The line is currently: #{index+1}.#{name}")
 end 
 return deli
 end
