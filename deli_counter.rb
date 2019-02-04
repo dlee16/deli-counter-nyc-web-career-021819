c8d2f568638d54ed011d2a55a7d71fcd9842f84d
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-04 16:03:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,6 @@ def take_a_number(deli,name)
   puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
 
-def now_serving(deli)
-  
+def now_serving(deli,name)
+  "Currently serving #{name}."
 end
