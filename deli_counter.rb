c244e252ca1d67883eae87d2b2bb37fe0d74e435
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-04 15:16:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 def line(person)
 deli =[]
   if deli.length < person.length
-    return ""
-  person.each_with_index do |name, index|
+    return "The line is currently empty."
+  person.each.with_index(1) do |name, index|
     deli.push("The line is currently")
 end
 return "The line is currently" + deli
