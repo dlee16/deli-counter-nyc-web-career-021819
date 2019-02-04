de9b6509d9563ce5fd674ea3cc4a745b1d38b02b
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-04 15:57:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ puts "The line is currently empty."
 end
 end 
 
-def take_a_number(deli)
+def take_a_number(deli,name)
   deli.each_with_index do |name, index|
   deli.push("Welcome, #{name} You are number #{index +1} in line.")
 end
